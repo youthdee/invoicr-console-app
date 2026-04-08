7f6f66d82ed5d394f402863401d7ab4a1ae37976
youthdee <k.veselyyy@email.cz>
2026-04-08 09:56:59 +0200

    doc

--- Invoicr.ConsoleApp/Managers/CsvManager.cs
+++ Invoicr.ConsoleApp/Managers/CsvManager.cs
@@ -2,15 +2,21 @@ using System.Text;
 
 namespace Invoicr.Managers;
 
-//Statická třída pro přímou práci s CSV soubory. 
-//Udělal jsem ji statickou, protože nechci vytvářet instanci. Třída je v paměti celou dobu a jenom jedna a ta samá,
-//Takže můžu s klidným vědomím volat její metody rovnou na třídě a ne provolávat metody/funkce objektu.
+/// <summary>
+/// Statická třída pro přímou práci s CSV soubory. 
+/// Udělal jsem ji statickou, protože nechci vytvářet instanci. Třída je v paměti celou dobu a jenom jedna a ta samá,
+/// Takže můžu s klidným vědomím volat její metody rovnou na třídě a ne provolávat metody/funkce objektu.
+/// </summary>
 public static class CsvManager
 {
     //CSV separátor, typicky středník
     const char Separator = ';';
 
-    //Helper metoda pro escapování
+    /// <summary>
+    /// Helper metoda pro escapování hodnot v jednotlivých sloupcích.
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
     public static string Escape(string? text)
     {
         if (text == null)
@@ -33,6 +39,12 @@ public static class CsvManager
         //pokud není splněná žádná z výše uvedených podmínek tak ten text vrátím
         return text;
     }
+
+    /// <summary>
+    /// Helper funkce, která parsuje celý řádek.
+    /// </summary>
+    /// <param name="line"></param>
+    /// <returns></returns>
 
     //Tato funkce byla vygenerována umělou inteligencí, konkrétně GEMINI PRO a prompt byl následující:
     //„Napiš v C# metodu ParseLine, která rozparsuje řádek CSV do pole stringů s ohledem na text v uvozovkách.
@@ -95,7 +107,11 @@ public static class CsvManager
         return fields.ToArray();
     }
 
-    //Jednoduchá metoda vracející "řádek", který se pak může rovnou uložit do CSV souboru.
+    /// <summary>
+    /// Jednoduchá metoda vracející "řádek", který se pak může rovnou uložit do CSV souboru.
+    /// </summary>
+    /// <param name="cols"></param>
+    /// <returns></returns>
     public static string Row(params string[] cols) =>
         string.Join(Separator, cols.Select(Escape));
 }
