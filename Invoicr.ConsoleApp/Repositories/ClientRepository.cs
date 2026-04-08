84b2ee808673dedbfeb223e586d70e998c7c40b9
youthdee <k.veselyyy@email.cz>
2026-04-08 10:32:16 +0200

    doc

--- Invoicr.ConsoleApp/Repositories/ClientRepository.cs
+++ Invoicr.ConsoleApp/Repositories/ClientRepository.cs
@@ -2,10 +2,15 @@ using Invoicr.Objects;
 
 namespace Invoicr.Repositories;
 
+/// <summary>
+/// Repozitář odběratelů. Dědí z base třídy Repository, díky generice využívá jejich metod a funkcionality.
+/// </summary>
 public class ClientRepository : Repository<Client>
 {
+    //nastavení názvu souboru ("databáze") do konstanty
     private const string file = "clients.csv";
 
+    //konstruktor předávající plnou cestu k CSV souboru zděděné třídě.
     public ClientRepository(string basePath) : base(Path.Combine(basePath, file))
     {
     }
--- Invoicr.ConsoleApp/Repositories/SupplierRepository.cs
+++ Invoicr.ConsoleApp/Repositories/SupplierRepository.cs
@@ -2,10 +2,15 @@ using Invoicr.Objects;
 
 namespace Invoicr.Repositories;
 
+/// <summary>
+/// Repozitář dodavatelů. Dědí z base třídy Repository, díky generice využívá jejich metod a funkcionality.
+/// </summary>
 public class SupplierRepository : Repository<Supplier>
 {
+    //nastavení názvu souboru ("databáze") do konstanty
     private const string file = "suppliers.csv";
 
+    //konstruktor předávající plnou cestu k CSV souboru zděděné třídě.
     public SupplierRepository(string basePath) : base(Path.Combine(basePath, file))
     {
     }
