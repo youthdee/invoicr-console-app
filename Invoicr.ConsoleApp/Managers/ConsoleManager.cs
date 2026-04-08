5f10889cab8f17e4f8618012b288fcdee14a8657
youthdee <k.veselyyy@email.cz>
2026-04-08 09:53:58 +0200

    doc

--- Invoicr.ConsoleApp/Managers/ConsoleManager.cs
+++ Invoicr.ConsoleApp/Managers/ConsoleManager.cs
@@ -4,6 +4,10 @@ namespace Invoicr.Managers;
 
 public static class ConsoleManager
 {
+    /// <summary>
+    /// Helper funkce, která vytvoří jednoduchý stylovaný header v konzoli.
+    /// </summary>
+    /// <param name="title"></param>
     public static void Header(string title)
     {
         Console.WriteLine();
@@ -12,12 +16,26 @@ public static class ConsoleManager
         Console.ResetColor();
     }
 
+    /// <summary>
+    /// Helper funkce pro menu výběr ve formátu [{číslo}] {Popis}
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="label"></param>
     public static void MenuItem(int n, string label)
         => Console.WriteLine($"  [{n}] {label}");
 
+    /// <summary>
+    /// Helper funkce pro oddělení itemů v konzoli. 40x vypíše znak "-" a poté odsadí.
+    /// </summary>
     public static void Separator()
         => Console.WriteLine("  " + new string('─', 40));
 
+    /// <summary>
+    /// Funkce pro přečtení volby, co uživatel zadal. Volba musí existovat a musí být notnull int.
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
     public static int ReadChoice(int min, int max)
     {
         while (true)
@@ -36,6 +54,14 @@ public static class ConsoleManager
         }
     }
 
+    /// <summary>
+    /// Helper funkce / "override" standardní metody Console.ReadLine.
+    /// Vylepšená verze, která zobrazuje default value. (default value se aplikuje při string.Empty)
+    /// Řětezec /q zruší operaci a vrátí null, s kterým se pak v programu pracuje.
+    /// </summary>
+    /// <param name="prompt"></param>
+    /// <param name="defaultVal"></param>
+    /// <returns></returns>
     public static string? ReadLine(string prompt, string? defaultVal = null)
     {
         var hint = defaultVal is not null ? $" [{defaultVal}]" : "";
@@ -46,6 +72,12 @@ public static class ConsoleManager
         return string.IsNullOrEmpty(val) ? (defaultVal ?? "") : val;
     }
 
+    /// <summary>
+    /// Nadstavba funkce ReadLine, která parsuje datetime.
+    /// </summary>
+    /// <param name="prompt"></param>
+    /// <param name="defaultVal"></param>
+    /// <returns></returns>
     public static DateTime? ReadDate(string prompt, DateTime? defaultVal = null)
     {
         var defStr = defaultVal?.ToString("yyyy-MM-dd");
@@ -65,6 +97,12 @@ public static class ConsoleManager
         }
     }
 
+    /// <summary>
+    /// Nadstavba funkce ReadLine, která parsuje Int.
+    /// </summary>
+    /// <param name="prompt"></param>
+    /// <param name="defaultVal"></param>
+    /// <returns></returns>
     public static int? ReadInt(string prompt, int? defaultVal = null)
     {
         var defStr = defaultVal?.ToString(CultureInfo.InvariantCulture);
@@ -83,6 +121,12 @@ public static class ConsoleManager
         }
     }
 
+    /// <summary>
+    /// Nadstavba funkce ReadLine, která parsuje Decimal
+    /// </summary>
+    /// <param name="prompt"></param>
+    /// <param name="defaultVal"></param>
+    /// <returns></returns>
     public static decimal? ReadDecimal(string prompt, decimal? defaultVal = null)
     {
         var defStr = defaultVal?.ToString(CultureInfo.InvariantCulture);
@@ -101,6 +145,12 @@ public static class ConsoleManager
         }
     }
 
+    /// <summary>
+    /// Nadstavba funkce ReadLine, která parsuje Long.
+    /// </summary>
+    /// <param name="prompt"></param>
+    /// <param name="defaultVal"></param>
+    /// <returns></returns>
     public static long? ReadLong(string prompt, decimal? defaultVal = null)
     {
         var defStr = defaultVal?.ToString(CultureInfo.InvariantCulture);
@@ -119,6 +169,10 @@ public static class ConsoleManager
         }
     }
 
+    /// <summary>
+    /// Helper funkce pro vypsání success zprávy.
+    /// </summary>
+    /// <param name="msg"></param>
     public static void Success(string msg)
     {
         Console.ForegroundColor = ConsoleColor.Green;
@@ -126,6 +180,10 @@ public static class ConsoleManager
         Console.ResetColor();
     }
 
+    /// <summary>
+    /// Helper funkce pro vypsání chybové hlášky.
+    /// </summary>
+    /// <param name="msg"></param>
     public static void Error(string msg)
     {
         Console.ForegroundColor = ConsoleColor.Red;
@@ -133,10 +191,20 @@ public static class ConsoleManager
         Console.ResetColor();
     }
 
+    /// <summary>
+    /// Helper funkce pro vypsání informační zprávy.
+    /// </summary>
+    /// <param name="msg"></param>
     public static void Info(string msg)
         => Console.WriteLine($"  {msg}");
 
-    private static void PrintHeader(string title, string columns, int width)
+    /// <summary>
+    /// Privátní funkce pro vypsání záhlaví tabulky.
+    /// </summary>
+    /// <param name="title"></param>
+    /// <param name="columns"></param>
+    /// <param name="width"></param>
+    private static void PrintTableHeader(string title, string columns, int width)
     {
         Console.WriteLine($"\n>>> {title.ToUpper()} <<<");
         Console.WriteLine(new string('-', width));
@@ -144,13 +212,22 @@ public static class ConsoleManager
         Console.WriteLine(new string('-', width));
     }
 
+    /// <summary>
+    /// Funkce pro vypsání kompletní tabulky.
+    /// </summary>
+    /// <param name="title"></param>
+    /// <param name="items"></param>
+    /// <param name="header"></param>
+    /// <param name="width"></param>
+    /// <typeparam name="T"></typeparam>
     public static void PrintTable<T>(string title, List<T> items, string header, int width)
     {
-        PrintHeader(title, header, width);
+        PrintTableHeader(title, header, width);
         foreach (var item in items)
         {
             Console.WriteLine(item?.ToString());
         }
+
         Console.WriteLine(new string('-', width));
     }
 }
